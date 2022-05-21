63128fd06a45db9ad9c16025c93fba49da9d9c23
PikaIANG <tsoorckanukris@gmail.com>
2022-05-21 16:03:40 +0300

    fixed ground controller bug on multiple colliders

--- Game/Assets/Source/PlayerController/GroundController.cs
+++ Game/Assets/Source/PlayerController/GroundController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Source.PlayerController
@@ -5,16 +6,17 @@ namespace Source.PlayerController
     [RequireComponent(typeof(BoxCollider2D))]
     public class GroundController : MonoBehaviour
     {
-        public bool IsGrounded { get; private set; }
+        public bool IsGrounded => _currentColliders.Count != 0;
+        private readonly HashSet<Collider2D> _currentColliders = new HashSet<Collider2D>();
 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            IsGrounded = true;
+            _currentColliders.Add(col);
         }
 
         private void OnTriggerExit2D(Collider2D col)
         {
-            IsGrounded = false;
+            _currentColliders.Remove(col);
         }
     }
 }
