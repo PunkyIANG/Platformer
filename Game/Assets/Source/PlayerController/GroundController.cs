87fe86f82ada9d14708dc247086d183d3f079f16
PikaIANG <tsoorckanukris@gmail.com>
2022-08-05 16:01:08 +0300

    fixed collision layers
    - added separate layer for characters, so that they only collide with the static background
    - fixed ground control getting triggered on non static background

--- Game/Assets/Source/PlayerController/GroundController.cs
+++ Game/Assets/Source/PlayerController/GroundController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,14 +9,26 @@ namespace Source.PlayerController
     {
         public bool IsGrounded => _currentColliders.Count != 0;
         private readonly HashSet<Collider2D> _currentColliders = new HashSet<Collider2D>();
+        private int _currentLayer;
+
+        private void Start()
+        {
+            _currentLayer = gameObject.layer;
+        }
 
         private void OnTriggerEnter2D(Collider2D col)
         {
+            if (col.gameObject.layer != _currentLayer)
+                return;
+            
             _currentColliders.Add(col);
         }
 
         private void OnTriggerExit2D(Collider2D col)
         {
+            if (col.gameObject.layer != _currentLayer)
+                return;
+
             _currentColliders.Remove(col);
         }
     }
